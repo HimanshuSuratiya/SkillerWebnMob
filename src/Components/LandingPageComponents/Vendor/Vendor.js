0512ca90d3536452ed1efc30e46c1594e3cf3c93
Himanshu Suratiya <Himanshu84688@gmail.com>
2022-11-02 14:13:57 +0530

    Vendor Swiper fix

--- src/Components/LandingPageComponents/Vendor/Vendor.js
+++ src/Components/LandingPageComponents/Vendor/Vendor.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Images from "../../../Images/Image";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation, Pagination, Autoplay, Virtual } from "swiper/core";
-import "swiper/swiper-bundle.css";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { FreeMode, Pagination, Navigation } from "swiper";
 import DoneIcon from '@mui/icons-material/Done';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
-
-SwiperCore.use([Navigation, Pagination, Autoplay, Virtual]);
 
 const Vendor = () => {
     const CategoriesData = [
@@ -79,43 +78,6 @@ const Vendor = () => {
         },
     ]
 
-    const slides = CategoriesData.map((item, index) => {
-        return (
-            <SwiperSlide key={`slide-${index}`} style={{ listStyle: "none" }}>
-                <div className="swiper-slide swiper-slide-visible swiper-slide-active" data-swiper-slide-index="0" role="group" aria-label="1 / 4" style={{ width: "362px", marginRight: "30px" }}>
-                    <div className="aon-ow-provider-wrap">
-                        <div className="aon-ow-provider shine-hover">
-                            <div className="aon-ow-top">
-                                <div className="aon-pro-check"><span> <DoneIcon /></span></div>
-                                <div className="aon-pro-favorite"><a href="#"> <FavoriteBorderIcon /></a></div>
-                                <div className="aon-ow-info">
-                                    <h4 className="aon-title"><a href="#!">Mila Kunis</a></h4>
-                                    <span>Queens, United States</span>
-                                </div>
-                            </div>
-                            <div className="aon-ow-mid">
-                                <div className="aon-ow-media media-bg-animate">
-                                    <a className="shine-box" href="#!"><img src={item.imgSource} alt="" /></a>
-                                </div>
-                                <p>Through our expertise, technological knowledge, global presence and bespoke.</p>
-                                <div className="aon-ow-pro-rating">
-                                    <span><StarBorderIcon /></span>
-                                    <span><StarBorderIcon /></span>
-                                    <span><StarBorderIcon /></span>
-                                    <span><StarBorderIcon /></span>
-                                    <span><StarBorderIcon /></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="aon-ow-bottom">
-                            <a href="profile-full.html">Request A Quote</a>
-                        </div>
-                    </div>
-                </div>
-            </SwiperSlide>
-        )
-    })
-
     return (
         <section className="site-bg-gray aon-feature-provider-area sf-curve-pos">
             <div className="container">
@@ -133,28 +95,54 @@ const Vendor = () => {
                 <div className="service-slider-one swiper-container style2 swiper-initialized swiper-horizontal swiper-pointer-events swiper-free-mode">
                     <div className="animationVendor" id="swiper-wrapper-service" aria-live="off">
                         <Swiper
-                            id="swiper"
-                            virtual
                             slidesPerView={3}
                             spaceBetween={30}
+                            freeMode={true}
                             autoplay
-                            onReachEnd={() => {
-                                const tmp = slides.unshift();
-                                slides.push(tmp);
+                            pagination={{
+                                dynamicBullets: true,
                             }}
-                            // navigation
-                            pagination
+                            navigation={true}
+                            modules={[FreeMode, Pagination, Navigation]}
+                            className="mySwiper"
                         >
-                            {slides}
+                            {CategoriesData.map((item, index) => {
+                                return (
+                                    <SwiperSlide >
+                                        <div className="swiper-slide swiper-slide-visible swiper-slide-active" data-swiper-slide-index="0" role="group" aria-label="1 / 4" style={{ width: "362px", marginRight: "30px" }}>
+                                            <div className="aon-ow-provider-wrap">
+                                                <div className="aon-ow-provider shine-hover">
+                                                    <div className="aon-ow-top">
+                                                        <div className="aon-pro-check"><span> <DoneIcon /></span></div>
+                                                        <div className="aon-pro-favorite"><a href="#"> <FavoriteBorderIcon /></a></div>
+                                                        <div className="aon-ow-info">
+                                                            <h4 className="aon-title"><a href="#!">Mila Kunis</a></h4>
+                                                            <span>Queens, United States</span>
+                                                        </div>
+                                                    </div>
+                                                    <div className="aon-ow-mid">
+                                                        <div className="aon-ow-media media-bg-animate">
+                                                            <a className="shine-box" href="#!"><img src={item.imgSource} alt="" /></a>
+                                                        </div>
+                                                        <p>Through our expertise, technological knowledge, global presence and bespoke.</p>
+                                                        <div className="aon-ow-pro-rating">
+                                                            <span><StarBorderIcon /></span>
+                                                            <span><StarBorderIcon /></span>
+                                                            <span><StarBorderIcon /></span>
+                                                            <span><StarBorderIcon /></span>
+                                                            <span><StarBorderIcon /></span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="aon-ow-bottom">
+                                                    <a href="profile-full.html">Request A Quote</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                )
+                            })}
                         </Swiper>
-                    </div>
-                </div>
-                <div className="slider-btn-wrap">
-                    <div className="service-prev slider-btn style2" tabIndex="0" role="button" aria-label="Previous slide" aria-controls="swiper-wrapper-service">
-                        <i><ArrowBackIcon style={{ cursor: 'pointer' }} /></i>
-                    </div>
-                    <div className="service-next slider-btn style2" tabIndex="0" role="button" aria-label="Next slide" aria-controls="swiper-wrapper-service">
-                        <i><ArrowForwardIcon style={{ cursor: 'pointer' }} /></i>
                     </div>
                 </div>
             </div>
