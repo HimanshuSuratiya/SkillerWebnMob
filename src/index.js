955c91955bad451854243c06780a90528015ded4
Himanshu Suratiya <Himanshu84688@gmail.com>
2022-11-22 18:09:43 +0530

    Server configuration to run project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ScrollToTop from "./ScrollToTop";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/design_website/skillerwebnmob">
       <ScrollToTop />
       <App />
     </BrowserRouter>
