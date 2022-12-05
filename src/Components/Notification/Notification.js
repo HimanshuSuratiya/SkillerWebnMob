dfcfdc7b5d9dd4cf2e1bd42ef6e8e0d1f175256e
Himanshu Suratiya <Himanshu84688@gmail.com>
2022-12-05 16:53:20 +0530

    Notification Page responsive

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -14,7 +14,7 @@ const Notification = () => {
             <section>
                 <Banner imgSource={Images.notification} text="Notification" />
                 <div className='p-4'>
-                    <div className='container'>
+                    <div className='container p-0'>
                         <div className="page-wrapper" style={{ minHeight: '250px' }}>
                             <div className="container-fluid">
                                 <div className="row">
@@ -42,8 +42,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
@@ -67,8 +67,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
@@ -92,8 +92,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
@@ -117,8 +117,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
@@ -142,8 +142,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
@@ -167,8 +167,8 @@ const Notification = () => {
                                                             </button>
                                                         </h2>
                                                         <div id="flush-collapseOne" className="accordion-collapse collapse show" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                                            <Accordion.Body>
-                                                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.
+                                                            <Accordion.Body className='notification-main-text-area'>
+                                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                                             </Accordion.Body>
                                                         </div>
                                                     </Accordion.Item>
